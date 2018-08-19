43f21952580a0445cb3ed0f8b2fbc7f56f55984d
momoka <454768497@qq.com>
2018-08-19 16:41:19 +0800

    H5ComponentBase参数设置

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -1,2 +1,24 @@
 /* 基本图文组件对象 */
+var H5ComponentBase=function ( name, cfg ) {
+    var cfg=cfg||{};
+    var id=( 'h5_c_'+Math.random() ).replace('.','_');
+    var cls='h5_component_'+cfg.type+' h5_component_name_'+name;
+    var component=$('<div class="h5_component '+cls+'" id="'+id+'">');
+
+    cfg.text && component.text(cfg.text);
+    cfg.width && component.width(cfg.width/2);
+    cfg.height && component.height(cfg.height/2);
+
+    cfg.css && component.css( cfg.css );
+    cfg.bg && component.css('backgroundImage','url('+cfg.bg+')');
+
+    if (cfg.center==true){
+        component.css({
+            marginLeft:(cfg.width/4 * -1)+'px',
+            left:'50%'
+        })
+    }
+
+    return component;
+}
 
